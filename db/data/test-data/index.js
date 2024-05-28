78fc942eccd7ba7857c8a11a611f7bbe41697cee
danielrice97 <danielrice@protonmail.ch>
2024-05-28 12:08:18 +0100

    question 1

--- db/data/test-data/index.js
+++ db/data/test-data/index.js
@@ -1,4 +1,7 @@
-exports.articleData = require('./articles.js');
-exports.commentData = require('./comments.js');
-exports.topicData = require('./topics.js');
-exports.userData = require('./users.js');
+module.exports = {
+    articleData: require('./articles.js'),
+    commentData: require('./comments.js'),
+    topicData: require('./topics.js'),
+    userData: require('./users.js')
+};
+
